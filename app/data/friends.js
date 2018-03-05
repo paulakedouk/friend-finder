9c235488f8a3433d59a637570e2641aea1041acc
paulakedouk <paulakedouk@gmail.com>
2018-03-04 21:26:17 -0800

    Adjust a big bug

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 var friends = [
   {
-    name: 'Rachel Green',
-    photo: '../public/assets/images/rachel.jpg',
-    scores: [2, 3, 5, 1, 3, 4, 2, 1, 5, 2]
-  },
-  {
     name: 'Monica Geller',
-    photo: '../public/assets/images/monica.jpg',
+    photo: 'assets/images/monica.jpg',
     scores: [5, 1, 3, 2, 4, 1, 3, 1, 5, 2]
   },
   {
+    name: 'Rachel Green',
+    photo: 'assets/images/rachel.jpg',
+    scores: [2, 3, 5, 1, 3, 4, 2, 1, 5, 2]
+  },
+  {
     name: 'Chandler Bing',
-    photo: '../public/assets/images/chandler.jpg',
+    photo: 'assets/images/chandler.jpg',
     scores: [2, 1, 2, 3, 4, 5, 1, 1, 2, 5]
   },
   {
     name: 'Phoebe Buffay',
-    photo: '../public/assets/images/phoebe.jpg',
+    photo: 'assets/images/phoebe.jpg',
     scores: [1, 1, 2, 1, 2, 3, 4, 2, 3, 1]
   },
   {
     name: 'Joey Tribbiani',
-    photo: '../public/assets/images/joey.jpg',
+    photo: 'assets/images/joey.jpg',
     scores: [3, 3, 2, 4, 5, 1, 2, 3, 5, 2]
   },
   {
     name: 'Ross Geller',
-    photo: '../public/assets/images/ross.jpg',
+    photo: 'assets/images/ross.jpg',
     scores: [1, 5, 2, 4, 1, 1, 3, 3, 4, 1]
   }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,45 +3,56 @@ var friends = require('../data/friends');
 module.exports = function(app) {
   app.get('/api/friends', function(req, response) {
     response.json(friends);
-    console.log(friends);
+    // console.log(friends);
   });
 
   app.post('/api/friends', function(req, res) {
-    var results = req.body;
-    console.log('results: ', results);
-
-    results.name = results.name.replace(/\s+/g, '');
-    results.photo = results.photo.replace(/\s+/g, '');
+    var currentUser = req.body;
+    // console.log('currentUser: ', currentUser);
 
     // Variables to store matched friend data
-    var matchName = '';
-    var matchImage = '';
-    var newScores = results.scores;
+    var match = {
+      name: '',
+      photo: '',
+      difference: 1000
+    };
 
-    var difference = 10000;
+    var matched;
+    var currentScores = currentUser['scores[]'];
 
-    for (var i = 0; i < friends.length; i++) {
-      var initialDiff = 0;
+    if (friends.length === 0) {
+      friends.push(currentUser);
+      res.send('Sorry! There is no one else for you to match with!');
+    } else {
+      for (var i = 0; i < friends.length; i++) {
+        var initialDiff = 0;
+        var storedScores = friends[i].scores;
+        // console.log('friends score: ', friends[i].scores);
 
-      console.log('friends score: ', friends[i].scores);
+        for (var j = 0; j < storedScores.length; j++) {
+          // I am not able to convert the string to a number, I tried to use parseInt, but it still not working.
+          // console.log('friend score: ', friendsScores);
 
-      for (var j = 0; j < friends[i].length; j++) {
-        // console.log('new score: ', newScores[j]);
-        //     initialDiff += Math.abs(friends[i].scores[h] - newScores[h]);
+          initialDiff += Math.abs(parseInt(storedScores[j]) - parseInt(currentScores[j]));
+        }
+
+        if (initialDiff < match.difference) {
+          match.name = friends[i].name;
+          // console.log(friends[i].name);
+
+          match.photo = friends[i].photo;
+          match.difference = initialDiff;
+          matched = friends[i];
+        }
       }
+      // console.log(currentUser);
+      friends.push(currentUser);
 
-      //   if (initialDiff < difference) {
-      //     difference = initialDiff;
-      //     matchName = friends[i].name;
-      //     matchImage = friends[i].photo;
-      //   }
+      var result = {
+        friendA: matched,
+        friendB: currentUser
+      };
+      res.json(result);
     }
-
-    // friends.push(results);
-    var result = [friends[0]];
-    res.json({
-      status: 'OK',
-      result
-    });
   });
 };
